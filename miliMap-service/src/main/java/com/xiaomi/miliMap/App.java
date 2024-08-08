07ab0303f53ab0808fd8102ac8bd1c81a3b87ebe
yinshuo <yinss020315@163.com>
2024-08-08 15:37:27 +0800

    fix

--- miliMap-service/src/main/java/com/xiaomi/miliMap/App.java
+++ miliMap-service/src/main/java/com/xiaomi/miliMap/App.java
@@ -1,14 +1,18 @@
 package com.xiaomi.miliMap;
 
+import com.xiaomi.aegis.constant.SdkConstants;
 import com.xiaomi.aegis.filter.AegisFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.Arrays;
+
 /**
- * @author hulonghai@xiaomi.com  2024/5/23
+ * @author yinshuo@xiaomi.com
  * description:
  */
 @EnableAsync
@@ -22,13 +26,31 @@ public class App {
         }
 
     }
-    @Bean
-    public FilterRegistrationBean filterCasBean() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(new AegisFilter());
-        registrationBean.setOrder(1);
-        registrationBean.addUrlPatterns("/*");
-        return registrationBean;
-    }
+//    @Bean
+//    public FilterRegistrationBean filterCasBean() {
+//        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+//        registrationBean.setFilter(new AegisFilter());
+//        registrationBean.setOrder(1);
+//        registrationBean.addUrlPatterns("/*");
+//        return registrationBean;
+//    }
+
+//    @Bean
+//    @Profile({"staging"})
+//    public FilterRegistrationBean<AegisFilter> filterCasStagingBean() {
+//        String ignoreUrl = String.join(",", Arrays.asList(
+//                "/api/NoLoginTest/*",
+//                "/api/process/*"
+//        ));
+//        FilterRegistrationBean<AegisFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new AegisFilter());
+//        registrationBean.addUrlPatterns("/*");
+//        registrationBean.addInitParameter(SdkConstants.PUBLIC_KEY_FILTER_INIT_PARAM_KEY, "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDOOnsiJpdPZKG8tU9hdgCTWyDn" +
+//                "KJSf/Du2Qd7Wo0zXuusTsJneS2XxNsTyqRFPEGpfEcJe5+cg8k7ZPpJu4p/2PcKZ" +
+//                "YWnK72f5XsZgEBw7eTD0sBtQBOF6y9NOTzPShwdm5woLmFKoj/XrR3BkwuMyw0/k" +
+//                "N4UvtSyTrfY/8WT7+QIDAQAB");
+//        registrationBean.addInitParameter(SdkConstants.IGNORE_URL_FILTER_INIT_PARAM_KEY,ignoreUrl);
+//        return registrationBean;
+//    }
 
 }
