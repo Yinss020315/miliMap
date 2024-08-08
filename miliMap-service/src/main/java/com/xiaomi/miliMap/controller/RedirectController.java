081e8bb7af9fb18b8603823d97d73632cceb59ca
yinshuo <yinss020315@163.com>
2024-08-09 00:19:03 +0800

    fix

--- miliMap-service/src/main/java/com/xiaomi/miliMap/controller/RedirectController.java
+++ miliMap-service/src/main/java/com/xiaomi/miliMap/controller/RedirectController.java
@@ -1,14 +1,10 @@
 package com.xiaomi.miliMap.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpRequest;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -19,18 +15,17 @@ import java.io.IOException;
  * @Date: 2024/8/8
  */
 @RestController
+@Log4j2
 @RequestMapping("/auth")
 public class RedirectController {
-
-    private static final Logger logger = LoggerFactory.getLogger(RedirectController.class);
 
     @RequestMapping(value = "/login",method = {RequestMethod.GET,RequestMethod.POST})
     void redirect(HttpServletResponse response, @RequestParam("currentPath") String currentPath) {
         try {
-            logger.info("path is :{}",currentPath);
+            log.info("currentPath:{}",currentPath);
             response.sendRedirect(currentPath);
         }catch (IOException e){
-            logger.error("error:",e);
+            log.error("error:",e);
         }
     }
 }
